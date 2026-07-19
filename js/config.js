83b07948daa3d9069434c3cc69fc231b17e9ef1c
yuki <asaxxhiii@gmail.com>
2026-07-19 09:59:49 +0530

    Update Supabase URL and anon key in config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
    ============================================================ */
 window.OPENHOUSE_DB = {
   // e.g. 'https://abcdefgh.supabase.co'
-  url: 'PASTE_YOUR_SUPABASE_URL_HERE',
+  url: 'https://llhmzicmgstlmbjjhope.supabase.co',
   // the long "anon / public" key (NOT the service_role key)
-  anonKey: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsaG16aWNtZ3N0bG1iampob3BlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0MjUzNzMsImV4cCI6MjEwMDAwMTM3M30.Q0vHLJeNwgRinehkf2pjij_vYThttna-Lm6dPm393x4'
 };
